0c7f19e485a299771f4f264bc83857d57f8729af
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-25 23:23:18 +0300

    related changes on urls and views.py

--- fscohort/views.py
+++ fscohort/views.py
@@ -27,3 +27,37 @@ def student_add(request):
         'form':form
     }
     return render(request, 'fscohort/student_add.html',context)
+
+
+def student_update(request, id):
+    student=Student.objects.get(id=id)
+    form=StudentForm(instance=student)
+    if request.method== "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+    context = {
+        'form':form,
+    }
+    return render(request, 'fscohort/student_update.html', context)
+
+
+def student_delete(request, id):
+    # student = get_object_or_404(Student, id=id)
+    student=Student.objects.get(id=id)
+    if request.method == "POST":
+        student.delete()
+        return redirect("list")
+    context={
+            'student' : student
+        }
+    return render(request, "fscohort/student_delete.html", context) 
+
+
+def student_detail(request, id):        
+    student = Student.objects.get(id=id)
+    context = {
+        'student': student
+    }
+    return render(request, 'fscohort/student_detail.html', context)
