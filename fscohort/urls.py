6564f8d1cdd36d4afa2b33e39a8090c6e155e1d3
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-25 22:13:07 +0300

    read and create functions

--- fscohort/urls.py
+++ fscohort/urls.py
@@ -1,8 +1,10 @@
 
-from django.urls import path, include
-from .views import index
+from django.urls import path
+from .views import index, student_list, student_add
 
 urlpatterns = [
     
     path ('', index, name='index'),
+    path ('list/', student_list, name='list'),
+    path ('add/', student_add, name='add'),
 ]
