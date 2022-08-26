edad0aed39e4ad547c3cbef4f14a39ff077619f0
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-26 19:54:04 +0300

    2 amendments ın list.html and views.py

--- fscohort/views.py
+++ fscohort/views.py
@@ -29,11 +29,11 @@ def student_add(request):
     return render(request, 'fscohort/student_add.html',context)
 
 
-def student_update(request, id):
+def student_update(request,id):
     student=Student.objects.get(id=id)
     form=StudentForm(instance=student)
-    if request.method== "POST":
-        form = StudentForm(request.POST, instance=student)
+    if request.method== 'POST':
+        form=StudentForm(request.POST, instance=student)
         if form.is_valid():
             form.save()
             return redirect("list")
@@ -43,21 +43,21 @@ def student_update(request, id):
     return render(request, 'fscohort/student_update.html', context)
 
 
-def student_delete(request, id):
+def student_delete(request,id):
     # student = get_object_or_404(Student, id=id)
     student=Student.objects.get(id=id)
-    if request.method == "POST":
+    if request.method == 'POST':
         student.delete()
         return redirect("list")
     context={
-            'student' : student
+            'student' :student
         }
     return render(request, "fscohort/student_delete.html", context) 
 
 
-def student_detail(request, id):        
+def student_detail(request,id):        
     student = Student.objects.get(id=id)
     context = {
-        'student': student
+        'student':student
     }
     return render(request, 'fscohort/student_detail.html', context)
